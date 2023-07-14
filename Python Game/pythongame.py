ece15d1e510b01401dbdcb5f4bb28b01e3520760
ryanetunkel <123666075+ryanetunkel@users.noreply.github.com>
2023-07-13 21:47:29 -0400

    janky jump

--- Python Game/pythongame.py
+++ Python Game/pythongame.py
@@ -4,6 +4,8 @@ from sys import exit
 window_width = 800
 window_height = 400
 window_size = (window_width,window_height)
+jump_button = pygame.K_SPACE
+shoot_button = pygame.MOUSEBUTTONDOWN
 
 pygame.init()
 screen = pygame.display.set_mode(window_size)
@@ -14,6 +16,7 @@ test_font = pygame.font.Font('Python Game/font/Pixeltype.ttf',50)
 sky_surf = pygame.image.load('Python Game/graphics/Background.png').convert_alpha()
 sky_surf = pygame.transform.scale(sky_surf,window_size)
 
+grass_top_y = 371
 ground_surf = pygame.image.load('Python Game/graphics/Grass.png').convert_alpha()
 ground_surf = pygame.transform.scale(ground_surf,window_size)
 
@@ -21,14 +24,14 @@ score_surf = test_font.render('Our Game', False, "#FCDC4D")
 score_rect = score_surf.get_rect(center = (window_width/2,50))
 
 harold_x_pos = 600
-harold_y_pos = 371
+harold_y_pos = grass_top_y
 harold_surf = pygame.image.load('Python Game/graphics/harold/harold1.png').convert_alpha()
 harold_rect = harold_surf.get_rect(midbottom = (harold_x_pos,harold_y_pos))
 
 wizard_surf = pygame.image.load('Python Game/graphics/wizard/wizard_idle1/sprite_00.png').convert_alpha()
 wizard_surf = pygame.transform.scale(wizard_surf,(128,128))
-wizard_rect = wizard_surf.get_rect(midbottom = (80,371))
-
+wizard_rect = wizard_surf.get_rect(midbottom = (80,grass_top_y))
+wizard_gravity = 0
 
 fireball_x_start = wizard_rect.right - 20
 fireball_y_start = wizard_rect.centery + 24
@@ -47,13 +50,12 @@ while True:
             exit() # breaks out of the while True loop
         # if event.type == pygame.MOUSEMOTION: # MOUSEMOTION, MOUSEBUTTONDOWN, MOUSEBUTTONUP
         #     if wizard_rect.collidepoint(event.pos): print ('collision')
-        if event.type == pygame.MOUSEBUTTONDOWN:
+        if event.type == shoot_button:
             fireball_x_speed = 4
         if event.type == pygame.KEYDOWN:
-            if event.key == pygame.K_SPACE:
-                print('jump')
-        if event.type == pygame.KEYUP:
-            print('key up')
+            if event.key == jump_button:
+                wizard_gravity = -20
+
 
     screen.blit(sky_surf,(0,0))
     screen.blit(ground_surf,(0,0))
@@ -75,6 +77,9 @@ while True:
     if harold_rect.right <= 0: harold_rect.left = window_width
     screen.blit(harold_surf,harold_rect)
     # print(wizard_rect.left) # can be used to get positions
+
+    wizard_gravity += 1
+    wizard_rect.y += wizard_gravity
     screen.blit(wizard_surf,wizard_rect)
     screen.blit(fireball_surf,fireball_rect)
 
