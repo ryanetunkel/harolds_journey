c29406b92cfb2b8153aa2c6ecb6db25bf2ce36bc
ryanetunkel <ryanetunkel@gmail.com>
2024-03-23 15:01:12 -0500

    health display

--- harolds_journey/__main__.py
+++ harolds_journey/__main__.py
@@ -19,7 +19,7 @@ from graphics.health_bar.outline_health_bar import *
 def display_score():
     temp_additional_score = wizard.sprite.get_additional_score()
     current_time = int(pygame.time.get_ticks() / 1000) - start_time
-    score_surf = test_font.render(str(current_time + temp_additional_score), False, '#FCDC4D')
+    score_surf = test_font.render(str(current_time + temp_additional_score), False, "#FCDC4D")
     score_rect = score_surf.get_rect(center = (WINDOW_WIDTH/2,WINDOW_HEIGHT/8))
     screen.blit(score_surf,score_rect)
     return current_time + temp_additional_score
@@ -27,47 +27,47 @@ def display_score():
 
 def display_stats():
     # Health
-    health_stat_image_surf = pygame.image.load('harolds_journey/graphics/pickups/piercing/piercing_pickup.png').convert_alpha()
+    health_stat_image_surf = pygame.image.load("harolds_journey/graphics/wizard/wizard_health/heart.png").convert_alpha()
     health_stat_image_surf = pygame.transform.scale_by(health_stat_image_surf,4 * (WINDOW_WIDTH + WINDOW_HEIGHT)/1200)
-    health_stat_image_rect = health_stat_image_surf.get_rect(center = (WINDOW_WIDTH*20/32,WINDOW_HEIGHT*5/32))
+    health_stat_image_rect = health_stat_image_surf.get_rect(center = (WINDOW_WIDTH*3/32,WINDOW_HEIGHT*3/32))
 
-    health_stat_surf = test_font.render('Piercing: ' + str(wizard.sprite.get_wizard_piercing_total() - 1), False, '#FCDC4D')
-    health_stat_surf = pygame.transform.scale_by(health_stat_surf, 0.9)
-    health_stat_rect = health_stat_surf.get_rect(center = (WINDOW_WIDTH*47/64,WINDOW_HEIGHT*5/32))
+    health_stat_surf = test_font.render(str(wizard.sprite.get_wizard_current_health()), False, "#FCDC4D")
+    health_stat_surf = pygame.transform.scale_by(health_stat_surf, 1)
+    health_stat_rect = health_stat_surf.get_rect(center = (WINDOW_WIDTH*9/64,WINDOW_HEIGHT*3/32))
 
     # Stat image Surfs - find a centralized place to keep all images so don't have to update this and the pickup class' version of the image
     # Damage
-    damage_stat_image_surf = pygame.image.load('harolds_journey/graphics/pickups/damage/damage_pickup.png').convert_alpha()
+    damage_stat_image_surf = pygame.image.load("harolds_journey/graphics/pickups/damage/damage_pickup.png").convert_alpha()
     damage_stat_image_surf = pygame.transform.scale_by(damage_stat_image_surf,4 * (WINDOW_WIDTH + WINDOW_HEIGHT)/1200)
     damage_stat_image_rect = damage_stat_image_surf.get_rect(center = (WINDOW_WIDTH*20/32,WINDOW_HEIGHT/16))
 
-    damage_stat_surf = test_font.render('Damage: ' + str(wizard.sprite.get_wizard_damage_total()), False, '#FCDC4D')
+    damage_stat_surf = test_font.render("Damage: " + str(wizard.sprite.get_wizard_damage_total()), False, "#FCDC4D")
     damage_stat_surf = pygame.transform.scale_by(damage_stat_surf, 0.9)
     damage_stat_rect = damage_stat_surf.get_rect(center = (WINDOW_WIDTH*93/128,WINDOW_HEIGHT/16))
 
     # Piercing
-    piercing_stat_image_surf = pygame.image.load('harolds_journey/graphics/pickups/piercing/piercing_pickup.png').convert_alpha()
+    piercing_stat_image_surf = pygame.image.load("harolds_journey/graphics/pickups/piercing/piercing_pickup.png").convert_alpha()
     piercing_stat_image_surf = pygame.transform.scale_by(piercing_stat_image_surf,4 * (WINDOW_WIDTH + WINDOW_HEIGHT)/1200)
     piercing_stat_image_rect = piercing_stat_image_surf.get_rect(center = (WINDOW_WIDTH*20/32,WINDOW_HEIGHT*5/32))
 
-    piercing_stat_surf = test_font.render('Piercing: ' + str(wizard.sprite.get_wizard_piercing_total() - 1), False, '#FCDC4D')
+    piercing_stat_surf = test_font.render("Piercing: " + str(wizard.sprite.get_wizard_piercing_total() - 1), False, "#FCDC4D")
     piercing_stat_surf = pygame.transform.scale_by(piercing_stat_surf, 0.9)
     piercing_stat_rect = piercing_stat_surf.get_rect(center = (WINDOW_WIDTH*47/64,WINDOW_HEIGHT*5/32))
 
     # Fireball Cooldown Stat
     # Fix text
-    fireball_cooldown_stat_image_surf = pygame.image.load('harolds_journey/graphics/pickups/fireball_cooldown/fireball_cooldown_pickup.png').convert_alpha()
+    fireball_cooldown_stat_image_surf = pygame.image.load("harolds_journey/graphics/pickups/fireball_cooldown/fireball_cooldown_pickup.png").convert_alpha()
     fireball_cooldown_stat_image_surf = pygame.transform.scale_by(fireball_cooldown_stat_image_surf,4 * (WINDOW_WIDTH + WINDOW_HEIGHT)/1200)
     fireball_cooldown_stat_image_rect = fireball_cooldown_stat_image_surf.get_rect(center = (WINDOW_WIDTH*20/32,WINDOW_HEIGHT*8/32))
 
-    fireball_cooldown_stat_surf = test_font.render(f'Cooldown: {round(wizard.sprite.get_max_fireball_cooldown_time()/60, 2)}', False, '#FCDC4D')
+    fireball_cooldown_stat_surf = test_font.render(f"Cooldown: {round(wizard.sprite.get_max_fireball_cooldown_time()/60, 2)}", False, "#FCDC4D")
     fireball_cooldown_stat_surf = pygame.transform.scale_by(fireball_cooldown_stat_surf, 0.9)
     fireball_cooldown_stat_rect = fireball_cooldown_stat_surf.get_rect(center = (WINDOW_WIDTH*95/128,WINDOW_HEIGHT*8/32))
 
     # Fireball Cooldown Icon
     fireball_cooldown_x_pos = WINDOW_WIDTH * 3/8
     fireball_cooldown_y_pos = WINDOW_HEIGHT * 11/64
-    fireball_cooldown_surf = pygame.image.load('harolds_journey/graphics/fireball/fireball_movement_animation/fireball_movement_00.png').convert_alpha()
+    fireball_cooldown_surf = pygame.image.load("harolds_journey/graphics/fireball/fireball_movement_animation/fireball_movement_00.png").convert_alpha()
     fireball_cooldown_rect = fireball_cooldown_surf.get_rect(center = (fireball_cooldown_x_pos,fireball_cooldown_y_pos))
     # Fireball Cooldown Overlay
     current_fireball_cooldown = wizard.sprite.get_current_fireball_cooldown()
@@ -82,6 +82,9 @@ def display_stats():
     fireball_cooldown_overlay_surf.fill(fireball_cooldown_overlay_color)
     fireball_cooldown_overlay_surf.set_alpha(100)
     # Blits
+    screen.blit(health_stat_image_surf,health_stat_image_rect)
+    screen.blit(health_stat_surf,health_stat_rect)
+
     screen.blit(damage_stat_image_surf,damage_stat_image_rect)
     screen.blit(damage_stat_surf,damage_stat_rect)
 
@@ -146,11 +149,11 @@ def obstacle_and_player_owned_projectile_collision():
                     if (temp_obstacle_health - temp_projectile_damage) <= 0:
                         # Pickup Spawn
                         if randint(1,5) == 5: # Chance to drop damage pickup
-                            pickup_group.add(Pickup('damage',temp_obstacle_x_pos,temp_obstacle_y_pos))
+                            pickup_group.add(Pickup("damage",temp_obstacle_x_pos,temp_obstacle_y_pos))
                         if randint(1,10) == 10: # Chance to drop fireball cooldown pickup
-                            pickup_group.add(Pickup('fireball_cooldown',temp_obstacle_x_pos,temp_obstacle_y_pos))
+                            pickup_group.add(Pickup("fireball_cooldown",temp_obstacle_x_pos,temp_obstacle_y_pos))
                         if randint(1,20) == 20: # Chance to drop piercing pickup
-                            pickup_group.add(Pickup('piercing',temp_obstacle_x_pos,temp_obstacle_y_pos))
+                            pickup_group.add(Pickup("piercing",temp_obstacle_x_pos,temp_obstacle_y_pos))
                         temp_additional_score += obstacle.get_points()
                         # Health Bar and Outline Health Bar Cleanup
                         old_health_bar = health_bar_ownership_group[obstacle]
@@ -180,11 +183,11 @@ def player_and_pickup_collision():
             temp_damage = wizard.sprite.get_wizard_damage_percent()
             temp_piercing = wizard.sprite.get_wizard_piercing_increase()
             temp_max_fireball_cooldown_time = wizard.sprite.get_max_fireball_cooldown_time()
-            if pickup.get_type() == 'damage':
+            if pickup.get_type() == "damage":
                 wizard.sprite.set_wizard_damage_percent(temp_damage + temp_bonus)
-            if pickup.get_type() == 'piercing':
+            if pickup.get_type() == "piercing":
                 wizard.sprite.set_wizard_piercing_increase(temp_piercing + temp_bonus)
-            if pickup.get_type() == 'fireball_cooldown' and temp_max_fireball_cooldown_time >= 6:
+            if pickup.get_type() == "fireball_cooldown" and temp_max_fireball_cooldown_time >= 6:
                 wizard.sprite.set_max_fireball_cooldown_time(temp_max_fireball_cooldown_time - temp_bonus)
             pygame.sprite.spritecollide(wizard.sprite,pickup_group,True)
 
@@ -211,33 +214,33 @@ outline_health_bar_ownership_group = {pygame.sprite.Sprite(): pygame.sprite.Spri
 
 moving_sprites = [wizard, harold, obstacle_group, projectile_group, pickup_group, outline_health_bar_group, health_bar_group,]
 
-sky_surf = pygame.image.load('harolds_journey/graphics/Background.png').convert_alpha()
+sky_surf = pygame.image.load("harolds_journey/graphics/Background.png").convert_alpha()
 sky_surf = pygame.transform.scale(sky_surf,WINDOW_SIZE)
 
-ground_surf = pygame.image.load('harolds_journey/graphics/Grass.png').convert_alpha()
+ground_surf = pygame.image.load("harolds_journey/graphics/Grass.png").convert_alpha()
 ground_surf = pygame.transform.scale(ground_surf,WINDOW_SIZE)
 
 # Intro Screen
 wizard_title_start_x_pos = WINDOW_WIDTH / 2
 wizard_title_start_y_pos = WINDOW_HEIGHT * 3/4
-wizard_title_surf = pygame.image.load('harolds_journey/graphics/wizard/wizard_idle_animation/wizard_idle_00.png').convert_alpha()
+wizard_title_surf = pygame.image.load("harolds_journey/graphics/wizard/wizard_idle_animation/wizard_idle_00.png").convert_alpha()
 wizard_title_surf = pygame.transform.scale(wizard_title_surf,(WIZARD_WIDTH * ((WINDOW_WIDTH + WINDOW_HEIGHT)/1200), WIZARD_HEIGHT * ((WINDOW_WIDTH + WINDOW_HEIGHT)/1200)))
 wizard_title_rect = wizard_title_surf.get_rect(center = (wizard_title_start_x_pos,wizard_title_start_y_pos))
 
 harold_title_start_x_pos = wizard_title_rect.centerx
 harold_title_start_y_pos = wizard_title_rect.top - (52/4 * PIXEL_SIZE)
-harold_title_surf = pygame.image.load('harolds_journey/graphics/harold/harold_idle_animation/harold_idle_00.png').convert_alpha()
+harold_title_surf = pygame.image.load("harolds_journey/graphics/harold/harold_idle_animation/harold_idle_00.png").convert_alpha()
 harold_title_surf = pygame.transform.scale_by(harold_title_surf,(2.25 * ((WINDOW_WIDTH + WINDOW_HEIGHT)/1200)))
 harold_title_rect = harold_title_surf.get_rect(midbottom = (harold_title_start_x_pos,harold_title_start_y_pos))
 
-title_game_name_surf = test_font.render('Harold\'s Journey',False,"#FCDC4D")
+title_game_name_surf = test_font.render("Harold\'s Journey",False,"#FCDC4D")
 title_game_name_surf = pygame.transform.scale_by(title_game_name_surf,((WINDOW_WIDTH + WINDOW_HEIGHT)/1200))
 title_game_name_rect = title_game_name_surf.get_rect(center = (WINDOW_WIDTH/2,((70/400) * WINDOW_HEIGHT)))
 
 title_info_start_x_pos = wizard_title_rect.centerx
 title_info_start_y_pos = wizard_title_rect.centery + ((40/400) * WINDOW_HEIGHT)
 title_info_start_pos = (title_info_start_x_pos,title_info_start_y_pos)
-title_info_surf = test_font.render('Press any key or click to Start',False,"#FCDC4D")
+title_info_surf = test_font.render("Press any key or click to Start",False,"#FCDC4D")
 title_info_surf = pygame.transform.scale_by(title_info_surf,((WINDOW_WIDTH + WINDOW_HEIGHT)/1200))
 title_info_rect = title_info_surf.get_rect(center = (title_info_start_pos))
 
@@ -255,7 +258,7 @@ while True:
         if game_active:
             # Obstacle Timer Event Detection
             if event.type == obstacle_timer:
-                new_obstacle = Obstacle(choice(['bird','skeleton','skeleton','skeleton']),int(pygame.time.get_ticks() / 1000) - start_time)
+                new_obstacle = Obstacle(choice(["bird","skeleton","skeleton","skeleton"]),int(pygame.time.get_ticks() / 1000) - start_time)
                 obstacle_group.add(new_obstacle)
                 # Health Bar
                 new_health_bar = HealthBar(new_obstacle, new_obstacle.get_current_health(), new_obstacle.get_max_health())
@@ -272,7 +275,7 @@ while True:
                     temp_max_fireball_cooldown_time = wizard.sprite.get_max_fireball_cooldown_time()
                     wizard.sprite.set_current_fireball_cooldown(temp_max_fireball_cooldown_time)
                     wizard.sprite.set_fireball_hit(False)
-                    projectile_group.add(Projectile('fireball', wizard))
+                    projectile_group.add(Projectile("fireball", wizard))
 
         else:
             if event.type == pygame.KEYDOWN or event.type == pygame.MOUSEBUTTONDOWN:
@@ -338,7 +341,7 @@ while True:
 
         harold_title_rect.midbottom = (harold_title_start_x_pos,harold_title_start_y_pos)
 
-        score_message_surf = test_font.render('Score: ' + str(score),False,"#FCDC4D")
+        score_message_surf = test_font.render("Score: " + str(score),False,"#FCDC4D")
         score_message_surf = pygame.transform.scale_by(score_message_surf,((WINDOW_WIDTH + WINDOW_HEIGHT)/1200))
         score_message_rect = score_message_surf.get_rect(center = (WINDOW_WIDTH/2,(100/800 * WINDOW_HEIGHT)))
 
