7db6acfa6fa36c86b852154e638b3b218a533901
ryanetunkel <ryanetunkel@gmail.com>
2024-03-23 14:32:52 -0500

    removed spaces

--- harolds_journey/graphics/health_bar/health_bar.py
+++ harolds_journey/graphics/health_bar/health_bar.py
@@ -5,7 +5,7 @@ from graphics.health_bar.health_bar_animation_holder import *
 class HealthBar(pygame.sprite.Sprite):
     def __init__(self, source: pygame.sprite.Sprite, current_health: int, max_health: int):
         super().__init__()
-        
+
         self.source = source
         self.x_pos = self.source.get_x_pos()
         self.INNER_WIDTH = 16
@@ -20,41 +20,41 @@ class HealthBar(pygame.sprite.Sprite):
         self.image = pygame.transform.scale(self.image,(self.INNER_WIDTH * 4, self.INNER_HEIGHT * 2))
         self.rect = self.image.get_rect(center = (inner_centerx,inner_centery))
         # self.moving_bar = pygame.draw.rect(screen, self.inner_color, (inner_left, inner_top, self.INNER_WIDTH, self.INNER_HEIGHT))
-        
+
     # Coords
     def get_x_pos(self):
-        return self.x_pos 
-    
+        return self.x_pos
+
     def set_x_pos(self, new_x_pos):
         self.x_pos = new_x_pos
-    
+
     def get_y_pos(self):
         return self.y_pos
-    
+
     def set_y_pos(self, new_y_pos):
         self.y_pos = new_y_pos
-    
+
     def get_current_health(self):
         return self.current_health
-    
+
     def set_current_health(self, new_current_health):
         self.current_health = new_current_health
-        
+
     def get_max_health(self):
         return self.max_health
-    
+
     def set_max_health(self, new_max_health):
         self.max_health = new_max_health
-    
+
     def get_health_percentage(self):
         return self.health_percentage
-    
+
     def set_health_percentage(self, new_health_percentage):
         self.health_percentage = new_health_percentage
-    
+
     def get_source(self):
         return self.source
-    
+
     def animation_state(self):
         inner_centerx = self.source.get_x_pos()
         inner_centery = self.source.get_y_pos() + (WIZARD_HEIGHT / 2) + (self.INNER_HEIGHT * 2)
@@ -67,9 +67,9 @@ class HealthBar(pygame.sprite.Sprite):
         # Calculating position
         # The size of it is always changing in fluctuation with the health_percentage
         self.image = pygame.transform.scale(self.image,(self.INNER_WIDTH * 4 * self.health_percentage, self.INNER_HEIGHT * 2))
-        self.rect = self.image.get_rect(center = (inner_centerx,inner_centery))      
+        self.rect = self.image.get_rect(center = (inner_centerx,inner_centery))
         # self.moving_bar = pygame.draw.rect(screen, self.inner_color, (inner_left, inner_top, self.INNER_WIDTH, self.INNER_HEIGHT))
-    
+
     def update(self):
         self.set_current_health(self.source.get_current_health())
         self.set_max_health(self.source.get_max_health())
@@ -78,7 +78,7 @@ class HealthBar(pygame.sprite.Sprite):
         if self.current_health <= 0:
             self.current_health = 0
         self.destroy()
-        
+
     def destroy(self):
         if self.current_health == 0:
             self.kill()
--- harolds_journey/graphics/health_bar/outline_health_bar.py
+++ harolds_journey/graphics/health_bar/outline_health_bar.py
@@ -5,7 +5,7 @@ from graphics.health_bar.health_bar_animation_holder import *
 class OutlineHealthBar(pygame.sprite.Sprite):
     def __init__(self, health_bar: pygame.sprite.Sprite, x_pos: int, y_pos: int):
         super().__init__()
-        
+
         self.health_bar = health_bar
         self.x_pos = x_pos
         self.OUTER_WIDTH = 18
@@ -17,35 +17,35 @@ class OutlineHealthBar(pygame.sprite.Sprite):
         self.image = pygame.transform.scale(self.image,(self.OUTER_WIDTH * 4, self.OUTER_HEIGHT * 2))
         self.rect = self.image.get_rect(center = (inner_centerx,inner_centery))
         # self.moving_bar = pygame.draw.rect(screen, self.inner_color, (inner_left, inner_top, self.INNER_WIDTH, self.INNER_HEIGHT))
-        
+
     # Coords
     def get_x_pos(self):
-        return self.x_pos 
-    
+        return self.x_pos
+
     def set_x_pos(self, new_x_pos):
         self.x_pos = new_x_pos
-    
+
     def get_y_pos(self):
         return self.y_pos
-    
+
     def set_y_pos(self, new_y_pos):
         self.y_pos = new_y_pos
-    
+
     def get_health_bar(self):
         return self.health_bar
-    
+
     def animation_state(self):
         temp_source = self.health_bar.get_source()
         inner_centerx = temp_source.get_x_pos()
         inner_centery = temp_source.get_y_pos() + (temp_source.get_height() / 2) + 8
         self.image = get_outline_health_bar()
         self.image = pygame.transform.scale(self.image,(self.OUTER_WIDTH * 4, self.OUTER_HEIGHT * 2))
-        self.rect = self.image.get_rect(center = (inner_centerx,inner_centery))      
-    
+        self.rect = self.image.get_rect(center = (inner_centerx,inner_centery))
+
     def update(self):
         self.animation_state()
         self.destroy()
-        
+
     def destroy(self):
         if self.health_bar.get_current_health() == 0:
             self.kill()
