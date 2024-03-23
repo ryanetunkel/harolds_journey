7d692aac8e0f355fa42433b20197c1be712cdefa
ryanetunkel <ryanetunkel@gmail.com>
2024-03-23 15:46:11 -0500

    remove excess spaces

--- harolds_journey/pickup.py
+++ harolds_journey/pickup.py
@@ -6,7 +6,7 @@ from global_vars import *
 class Pickup(pygame.sprite.Sprite):
     def __init__(self, type: str, x_pos: int, y_pos: int):
         super().__init__()
-        
+
         self.x_pos = x_pos
         self.y_pos = y_pos
         self.gravity_intensity = 1
@@ -14,7 +14,7 @@ class Pickup(pygame.sprite.Sprite):
         self.PICKUP_ANIMATION_SPEED = 0.2
         self.LIFETIME_LIMIT = 10 * 60
         self.lifetime = self.LIFETIME_LIMIT
-        
+
         if type == 'damage': # Percentages
             self.type = 'damage'
             self.bonus = 0.5
@@ -26,7 +26,7 @@ class Pickup(pygame.sprite.Sprite):
             self.bonus = 1
             piercing_pickup = pygame.image.load('harolds_journey/graphics/pickups/piercing/piercing_pickup.png').convert_alpha()
             self.frames = [piercing_pickup]
-        
+
         elif type == 'fireball_cooldown': # Flat increases?
             self.type = 'fireball_cooldown'
             self.bonus = 2
@@ -36,34 +36,34 @@ class Pickup(pygame.sprite.Sprite):
         self.animation_index = 0
         self.image = self.frames[self.animation_index]
         self.image = pygame.transform.scale_by(self.image,3)
-        self.rect = self.image.get_rect(center = (self.x_pos,self.y_pos)) 
+        self.rect = self.image.get_rect(center = (self.x_pos,self.y_pos))
 
     def get_bonus(self):
         return self.bonus
-    
+
     def get_type(self):
         return self.type
 
     def get_height(self):
         return self.rect.bottom - self.rect.top
-    
+
     def apply_gravity(self):
         self.gravity += self.gravity_intensity
         self.rect.y += self.gravity
         if self.rect.bottom >= GRASS_TOP_Y: self.rect.bottom = GRASS_TOP_Y
-    
-    def animation_state(self):   
+
+    def animation_state(self):
         self.animation_index += self.PICKUP_ANIMATION_SPEED
         if self.animation_index >= len(self.frames): self.animation_index = 0
-        
+
         self.image = self.frames[int(self.animation_index)]
         self.image = pygame.transform.scale_by(self.image,3)
-    
+
     def update(self):
         self.apply_gravity()
         self.animation_state()
         self.destroy()
-        
+
     def destroy(self):
         if self.lifetime <= 0:
             self.kill()
