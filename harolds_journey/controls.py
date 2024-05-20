f797ba498a73d6ba47f2699daacdd75648201003
ryanetunkel <ryanetunkel@gmail.com>
2024-05-19 22:12:35 -0500

    Add comments to use arrow keys

--- harolds_journey/controls.py
+++ harolds_journey/controls.py
@@ -3,7 +3,7 @@
 import pygame
 
 # Controls
-jump_button = pygame.K_SPACE
-right_button = pygame.K_d
-left_button = pygame.K_a
+jump_button = pygame.K_SPACE # K_SPACE
+right_button = pygame.K_d # K_d or K_RIGHT
+left_button = pygame.K_a # K_a o r K_LEFT
 shoot_button = pygame.MOUSEBUTTONDOWN
